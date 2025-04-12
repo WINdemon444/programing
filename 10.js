ba8a7850c4dbbcfd09bfa761e23bcf678a088951
WINdemon444 <windemon444@gmail.com>
2025-04-12 10:56:20 +0300

    wert drrr

---
+++ 10.js
@@ -0,0 +1,5 @@
+let games=['roblox','geometry dash','minecraft','level devil','asphalt 9']
+console.log(games)
+console.log(games[0])//print 1 element of the array
+console.log(games[3])
+console.log(games.length)//length of array
