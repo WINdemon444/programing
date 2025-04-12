849a13158e63478b217a247c760c554a9b00b4f8
WINdemon444 <windemon444@gmail.com>
2025-04-12 10:12:57 +0300

    uuiiegulieiu

---
+++ hm9.js
@@ -0,0 +1,5 @@
+// 🔹 Condition (eng): There is a computer that costs 1500. Тhe discount is 17%, calculate and print the new price.
+let computer=1500
+let newprice=computer/100*17
+computer-=newprice
+console.log(computer)
