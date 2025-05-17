692b79dcab288f2a7b18aba4fa5de05aaabc9ac4
WINdemon444 <windemon444@gmail.com>
2025-05-17 10:50:07 +0300

    Create 15.js

---
+++ 15.js
@@ -0,0 +1,16 @@
+let w="bee"
+console.log(w.toUpperCase())//we made all letters capital
+let o='COW'
+console.log(o.toLowerCase())//made all letters small
+// You have an array of words. Convert all words to uppercase and print the new array.
+
+let words = ["sun", "cloud", "rain"];
+for(let i=0;i<words.length;i++){
+  console.log(words[i].toUpperCase())
+}
+let words2=['nun','cOOlkidd','1x1x1x1']
+let result=[]
+for(let i=0;i<words2.length;i++){
+  result.push((words2[i].toUpperCase()))
+}
+console.log((result))
