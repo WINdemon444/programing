f7565f8cb7ff17683a2161b7e86b71b2ce6d210b
WINdemon444 <windemon444@gmail.com>
2025-05-24 10:13:23 +0300

    hm15.js

---
+++ hm15.js
@@ -0,0 +1,6 @@
+// let words = ["HELLO", "WORLD", "JAVASCRIPT"];
+// You have an array of words. Convert all words to lowercase and print the new array.
+let words = ["HELLO", "WORLD", "JAVASCRIPT"]
+for(let i=0;i<words.length;i++){
+console.log(words[i].toLowerCase())
+}
