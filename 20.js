44456047a26983f51d1873f4182fbe5360370770
WINdemon444 <windemon444@gmail.com>
2025-07-12 10:46:32 +0300

    lesson 20

---
+++ 20.js
@@ -0,0 +1,19 @@
+let numbers=[1,22,33,43,50,97,6,60,46,21]
+let w=numbers.map(element=>element*2)
+console.log(w)
+let words=["i",'want','to','play','roblox']
+let word=words.map(element=>element.toUpperCase())
+console.log(word)
+
+
+
+
+
+
+
+
+
+
+let names=['ira','dmytro','ivan']
+let n=names.map(element=>'hi '+element)
+console.log(n)
