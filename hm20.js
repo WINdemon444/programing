708d04ba5aba9c761a40aa4a164432103139766d
WINdemon444 <windemon444@gmail.com>
2025-08-09 10:45:13 +0300

    yyyyiygkygygyugkuy

---
+++ hm20.js
@@ -0,0 +1,6 @@
+// Task: Make all words UPPERCASE(using map method) and sort them
+
+// Given a list of exactly 6 words, convert all of them to uppercase and then sort them alphabetically.
+
+// const words = ["car", "bike", "train", "apple", "zebra", "bus"];
+console.log("hello")
